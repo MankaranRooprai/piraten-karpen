7c559e00b49b4522edd9b99036128eeab9b4f209
Mankaran Rooprai <mankaranrooprai@gmail.com>
2023-01-26 13:18:04 -0500

    F20. Now monkey mode can be activated.

--- src/main/java/pk/Game.java
+++ src/main/java/pk/Game.java
@@ -73,7 +73,7 @@ public class Game {
         System.out.println();
         LOGGER.info("This is an INFO level log message");
         LOGGER.trace("HELLO");
-        LOGGER.error("yo");
+        // LOGGER.error("yo");
         System.out.println("That's all folks!");
 
     }
